025c9a6827af39a7dc505c47a9d85f42b52b3277
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-01-02 16:17:10 +0000

    added docs and fixing launcher

---
+++ tools/launcher/backup/launcher.pas
@@ -0,0 +1,41 @@
+unit launcher;
+
+{$mode objfpc}{$H+}
+{$DEFINE debug}     // do this here or you can define a -dDEBUG in Project Options/Other/Custom Options, i.e. in a build mode so you can set up a Debug with leakview and a Default build mode
+
+
+interface
+
+uses
+  {$IFDEF debug}
+  CMem, SysUtils,
+  {$ENDIF}
+  Classes, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+begin
+   {$IFDEF DEBUG}
+  // Assuming your build mode sets -dDEBUG in Project Options/Other when defining -gh
+  // This avoids interference when running a production/default build without -gh
+
+  // Set up -gh output for the Leakview package:
+  if FileExists('heap.trc') then
+    DeleteFile('heap.trc');
+  //SetHeapTraceOutput('heap.trc');
+  {$ENDIF DEBUG}
+end.
+
